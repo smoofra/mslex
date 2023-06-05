da7fe0a8772f6c181834209f8d284c3c49c7d535
Lawrence D'Anna <larry@elder-gods.org>
2023-06-05 13:07:44 -0400

    Bump version: 1.0.1 → 1.1.0

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/smoofra/mslex',
-    version='1.0.1',
+    version='1.1.0',
     zip_safe=False,
 )
